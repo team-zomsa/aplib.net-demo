310df322debb47604a3adb90f23873c583ba280b
Quake <QuakeEye@users.noreply.GitHub.com>
2024-02-22 14:05:25 +0100

    test:

--- workflow-scripts/test-build/activate-unity-license.sh
+++ workflow-scripts/test-build/activate-unity-license.sh
@@ -2,8 +2,11 @@
 set -e
 
 # Dump license to a ulf file
-echo "$UNITY_LICENSE"
+# echo "$UNITY_LICENSE"
+
+echo $UNITY_PATH
 
 # Activate unity license
 # xvfb-run --auto-servernum "$UNITY_PATH" -batchmode -nographics -quit -logFile "-" -serial license.ulf
+"$UNITY_PATH" -batchmode -nographics -quit -logFile "-"
 $UNITY_PATH -batchmode -nographics -quit -logFile "-"
