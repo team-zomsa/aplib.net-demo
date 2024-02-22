48abbb0912895b3d9cd866cbc19ea20021dbde21
Quake <QuakeEye@users.noreply.GitHub.com>
2024-02-22 12:36:15 +0100

    test: Try something else...

--- workflow-scripts/test-build/activate-unity-license.sh
+++ workflow-scripts/test-build/activate-unity-license.sh
@@ -6,4 +6,4 @@ UNITY_LICENSE=$1
 UNITY_PATH=$2
 
 # Activate unity license
-xvfb-run --auto-servernum $UNITY_PATH -batchmode -nographics -quit -logFile "-" -serial $UNITY_LICENSE
+xvfb-run --auto-servernum "$UNITY_PATH" -batchmode -nographics -quit -logFile "-" -serial "$UNITY_LICENSE"
