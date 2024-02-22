f0104f787b6f18465555772351724ad0df38ddd8
Quake <QuakeEye@users.noreply.GitHub.com>
2024-02-22 12:55:39 +0100

    test: Change to running editor

--- workflow-scripts/test-build/activate-unity-license.sh
+++ workflow-scripts/test-build/activate-unity-license.sh
@@ -9,4 +9,5 @@ UNITY_PATH=$2
 echo "$UNITY_LICENSE" > license.ulf
 
 # Activate unity license
-xvfb-run --auto-servernum "$UNITY_PATH" -batchmode -nographics -quit -logFile "-" -serial license.ulf
+# xvfb-run --auto-servernum "$UNITY_PATH" -batchmode -nographics -quit -logFile "-" -serial license.ulf
+$UNITY_PATH -batchmode -nographics -quit -logFile "-" -serial license.ulf
