539addd2ea9bc30edd3afe20250bfc0a23caff45
Quake <QuakeEye@users.noreply.GitHub.com>
2024-02-22 11:43:14 +0100

    chore: Start restructure of pipeline file for clarity

---
+++ .github/workflows/test-build/install-unity-editor.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+# Download unity download keys
+wget -qO - https://hub.unity3d.com/linux/keys/public 
+gpg --dearmor 
+sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
+
+# Retrieve unity hub versions
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
+
+# Install unity hub and required packages
+sudo apt update
+sudo apt install unityhub
+sudo apt install -y libgconf-2-4 libglu1 libasound2 libgtk2.0-0 libgtk-3-0 libnss3 zenity xvfb libfuse2 at-spi2-core
+sudo apt update
+
+# Set up directory for the editor installation
+mkdir -p ./Unity/Hub/Editor
+
+# Install unity editor
+xvfb-run --auto-servernum unityhub --headless install-path --set $(pwd)/Unity/Hub/Editor
+xvfb-run --auto-servernum unityhub --headless install --version 2022.3.19f1 --changeset 244b723c30a6
+
+# List file structure (Should be removed before merge, reviewer: if this is still here, make it known!)
+ls -R
