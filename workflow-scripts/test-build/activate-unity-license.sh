89b892150e10d9c02c93f143e2efe6d4234ad2b9
Quake <QuakeEye@users.noreply.GitHub.com>
2024-02-22 12:24:39 +0100

    test: Create unity license activation file

---
+++ workflow-scripts/test-build/activate-unity-license.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+# Get unity license
+UNITY_LICENSE=$1
+UNITY_PATH=$2
+
+# Activate unity license
+xvfb-run --auto-servernum $UNITY_PATH -batchmode -nographics -quit -logFile "-" -serial $UNITY_LICENSE
