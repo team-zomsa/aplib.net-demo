860e02eb77d9410189e1d495d11448c20dd89ef1
Jens Steenmetz <jj.steenmetz@gmail.com>
2024-02-22 14:26:45 +0100

    test:

--- workflow-scripts/test-build/activate-unity-license.sh
+++ workflow-scripts/test-build/activate-unity-license.sh
@@ -3,7 +3,8 @@ set -e
 
 # Dump license to a ulf file
 echo "$UNITY_LICENSE_ROOT" > license.ulf
-cat license.ulf
+
+echo "$(<license.ulf)"
 
 # Activate unity license
 # xvfb-run --auto-servernum "$UNITY_PATH" -batchmode -nographics -quit -logFile "-" -serial license.ulf
