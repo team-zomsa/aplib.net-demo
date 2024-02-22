b554fc3323ecbaf573896bec2b389ccb1f5d7505
Quake <QuakeEye@users.noreply.GitHub.com>
2024-02-22 13:55:29 +0100

    test: Do not use params

--- workflow-scripts/test-build/activate-unity-license.sh
+++ workflow-scripts/test-build/activate-unity-license.sh
@@ -6,7 +6,7 @@ UNITY_LICENSE=$1
 UNITY_PATH=$2
 
 # Dump license to a ulf file
-echo "$UNITY_LICENSE" > license.ulf
+# echo "$UNITY_LICENSE" > license.ulf
 
 # Activate unity license
 # xvfb-run --auto-servernum "$UNITY_PATH" -batchmode -nographics -quit -logFile "-" -serial license.ulf
