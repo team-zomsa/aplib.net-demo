810382ed5f1c8d093220cb6e8461d38a74684c3d
Quake <QuakeEye@users.noreply.GitHub.com>
2024-02-22 14:09:12 +0100

    test:

--- workflow-scripts/test-build/activate-unity-license.sh
+++ workflow-scripts/test-build/activate-unity-license.sh
@@ -8,5 +8,8 @@ echo $UNITY_PATH
 
 # Activate unity license
 # xvfb-run --auto-servernum "$UNITY_PATH" -batchmode -nographics -quit -logFile "-" -serial license.ulf
-chmod +x "$UNITY_PATH"
-sudo "$UNITY_PATH" -batchmode -nographics -quit -logFile "-"
+# chmod +x "$UNITY_PATH"
+# sudo "$UNITY_PATH" -batchmode -nographics -quit -logFile "-"
+
+chmod +x Unity/Hub/Editor/2022.3.19f1/Editor/Unity
+sudo Unity/Hub/Editor/2022.3.19f1/Editor/Unity -batchmode -nographics -quit -logFile "-"
