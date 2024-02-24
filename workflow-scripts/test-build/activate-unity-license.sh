dc42a66afc6cb63878c21721b732af0686ef9869
Quake <QuakeEye@users.noreply.GitHub.com>
2024-02-24 18:10:43 +0100

    test: List files

--- workflow-scripts/test-build/activate-unity-license.sh
+++ workflow-scripts/test-build/activate-unity-license.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 set -e
 
-# Dump license to a ulf file
+# Dump $UNITY_LICENSE to a file
 echo "$UNITY_LICENSE" > license.ulf
-
-echo "$(<license.ulf)"
 
 # Activate unity license
 # xvfb-run --auto-servernum "$UNITY_PATH" -batchmode -nographics -quit -logFile "-" -serial license.ulf
@@ -14,4 +12,4 @@ echo "$(<license.ulf)"
 # ls -R Unity/Hub/Editor/2022.3.19f1/Editor/
 
 # chmod +x Unity/Hub/Editor/2022.3.19f1/Editor/Unity
-Unity/Hub/Editor/2022.3.19f1/Editor/Unity -batchmode -nographics -quit -logFile "-" -manualLicenseFile "$UNITY_LICENSE"
+Unity/Hub/Editor/2022.3.19f1/Editor/Unity -batchmode -nographics -quit -logFile "-" -manualLicenseFile license.ulf
