37fb3bdefdec3e09f52f3f65e5ac865699d23d5f
Joen van de Vorle <j.j.vandevorle@students.uu.nl>
2024-02-27 13:46:21 +0100

    feat: add slope movement

--- aplib.net-demo/Assets/Scripts/Player/Movement.cs
+++ aplib.net-demo/Assets/Scripts/Player/Movement.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] float speed = 5;
+    [SerializeField] float maxSpeed = 7;
+    [SerializeField] float acceleration = 100;
     Vector3 horizontalVelocity;
     Vector2 horizontalInput;
 
@@ -16,12 +18,15 @@ public class Movement : MonoBehaviour
     float gravity;
 
     [SerializeField] float jumpHeight = 7;
+    [SerializeField] float airMovementMultiplier = 0.5f;
     Vector3 verticalVelocity;
-    bool jumpStarted;
-    bool isGrounded;
+    bool jumpPressed;
 
     [SerializeField] LayerMask groundMask;
+    [SerializeField] float groundDrag;
+    [SerializeField] float slopeAngle = 40;
     float playerHeight;
+    bool isGrounded;
 
     Rigidbody rb;
     CapsuleCollider controller;
@@ -35,45 +40,83 @@ public class Movement : MonoBehaviour
     }
 
     void FixedUpdate() {
-        // Ground check by checking a sphere at the bottom of the player
+        // Ground check by checking a sphere at the bottom of the player, more consistent than ray
         Vector3 bottomPoint = transform.TransformPoint(controller.center - 0.5f * playerHeight * Vector3.up);
-        isGrounded = Physics.CheckSphere(bottomPoint, 0.1f, groundMask);
+        isGrounded = Physics.CheckSphere(bottomPoint, 0.15f, groundMask);
 
-        if (isGrounded) 
-            Debug.Log("Grounded");  
-        else {
-            // Custom gravity for player
-            actualGravityScale = rb.velocity.y < 0 ? fallingGravityScale : normalGravityScale;
-            rb.velocity += (actualGravityScale - 1) * gravity * Time.fixedDeltaTime * Vector3.up;   
-        }
-
-        HorizontalMovement();
-        VerticalMovement();
+        MovePlayer();
+        HandleJump();
     }   
     
-    void HorizontalMovement() {
+    void MovePlayer() {
         horizontalVelocity = transform.right * horizontalInput.x + transform.forward * horizontalInput.y;
-        // We can choose to do horizontal movement using AddForce or MovePosition
-        // rb.AddForce(speed * Time.fixedDeltaTime * horizontalVelocity, ForceMode.VelocityChange);
-        rb.MovePosition(rb.position + speed * Time.fixedDeltaTime * horizontalVelocity);
+        bool isOnSlope = OnSlope(out Vector3 directionOnSlope, out RaycastHit downHit);
+
+        if (isGrounded) {
+            rb.drag = groundDrag; 
+            Debug.Log("Grounded");
+            if (isOnSlope) {
+                rb.AddForce(maxSpeed * acceleration * Time.fixedDeltaTime * 1.5f * directionOnSlope);
+                // Apply gravity but perpendicular to the slope
+                rb.velocity += (actualGravityScale - 1) * gravity * Time.fixedDeltaTime * downHit.normal;
+            }
+            else {
+                rb.AddForce(maxSpeed * acceleration * Time.fixedDeltaTime * horizontalVelocity.normalized);
+            }   
+        }
+        else {
+            rb.drag = 0;  
+            rb.AddForce(maxSpeed * acceleration * Time.fixedDeltaTime * airMovementMultiplier * horizontalVelocity.normalized); 
+            // Custom gravity for player
+            actualGravityScale = rb.velocity.y < 0 ? fallingGravityScale : normalGravityScale;
+            rb.velocity += (actualGravityScale - 1) * gravity * Time.fixedDeltaTime * Vector3.up;
+        }
+
+        rb.useGravity = !isOnSlope;
+        LimitVelocity(isOnSlope);
     }
 
-    void VerticalMovement(){
-        if (jumpStarted) {
+    void LimitVelocity(bool isOnSlope){
+        // limiting speed on slope
+        if (isOnSlope) {   
+            if (rb.velocity.magnitude > maxSpeed)
+                rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+        else {
+            Vector3 rbHorizontalVelocity = new(rb.velocity.x, 0, rb.velocity.z);
+            if (rbHorizontalVelocity.magnitude > maxSpeed) {
+                Vector3 limitedVelocity = rbHorizontalVelocity.normalized * maxSpeed;
+                rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+            }
+        }
+        Debug.DrawRay(transform.position, rb.velocity, Color.red);
+    }
+
+    bool OnSlope(out Vector3 directionOnSlope, out RaycastHit downHit) {
+        if (Physics.Raycast(transform.position, Vector3.down, out downHit, playerHeight * 0.5f + 0.3f, groundMask)){
+            float angle = Vector3.Angle(Vector3.up, downHit.normal);
+            if (angle < slopeAngle && angle != 0) {
+                Debug.Log("On climbable slope: " + angle);
+                directionOnSlope = Vector3.ProjectOnPlane(horizontalVelocity, downHit.normal).normalized;
+                return true;
+            }
+        }
+        directionOnSlope = Vector3.zero;
+        return false;
+    }
+
+    void HandleJump(){
+        if (jumpPressed) {
             if (isGrounded) {
                 verticalVelocity.y = jumpHeight;
                 rb.AddForce(verticalVelocity, ForceMode.Impulse);
             }
-            jumpStarted = false;
+            jumpPressed = false;
         }
     }
 
-    public void ReceiveInput(Vector2 _input)
-    {
-        horizontalInput = _input;
-    }
+    public void ReceiveInput(Vector2 _input) => horizontalInput = _input;
 
-    public void OnJumpPressed() {
-        jumpStarted = true;
-    }
+    public void OnJumpPressed() => jumpPressed = true;
+    
 }
