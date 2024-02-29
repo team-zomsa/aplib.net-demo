bf278029e25781fa49c319299c374e9d01c6644a
Thijs Boerefijn <tboefijnb2@gmail.com>
2024-02-29 11:35:23 +0100

    Improve styling

--- aplib.net-demo/Assets/Scripts/GameManager.cs
+++ aplib.net-demo/Assets/Scripts/GameManager.cs
@@ -2,27 +2,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-  private static GameManager _instance;
+    private static GameManager _instance;
 
-  public static GameManager Instance
-  {
-    get
+    public static GameManager Instance
     {
-      if (_instance == null) Debug.LogError("Game Manager is null, first awake it!");
-      
-      return _instance;
+        get
+        {
+            if (_instance == null) Debug.LogError("Game Manager is null, first awake it!");
+            
+            return _instance;
+        }
     }
-  }
 
-  private void Awake()
-  {
-    // Setup singleton
-    if (_instance != null && _instance != this) {
-        Destroy(gameObject);
-    } else {
-        _instance = this;
+    private void Awake()
+    {
+        // Setup singleton
+        if (_instance != null && _instance != this) {
+            Destroy(gameObject);
+        } else {
+            _instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
     }
-    
-    DontDestroyOnLoad(gameObject);
-  }
 }
