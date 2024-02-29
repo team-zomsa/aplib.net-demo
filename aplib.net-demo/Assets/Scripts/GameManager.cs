f2cb7e357b356bbd81541784230d982ecedce6e3
Thijs Boerefijn <tboefijnb2@gmail.com>
2024-02-29 10:24:49 +0100

    Add GameManager

---
+++ aplib.net-demo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+public class GameManager : MonoBehaviour
+{
+  private static GameManager _instance;
+
+  public static GameManager Instance
+  {
+    get
+    {
+      if (_instance == null)
+      {
+        Debug.LogError("Game Manager is null");
+        _instance = FindObjectOfType<GameManager>();
+      }
+      
+      return _instance;
+    }
+  }
+
+  private void Awake()
+  {
+    _instance = this;
+  }
+}
