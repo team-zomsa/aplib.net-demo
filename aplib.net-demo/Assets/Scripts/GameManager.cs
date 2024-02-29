37ba0966f2b4d7b16e07fba4cbe21a01b3426048
Thijs Boerefijn <tboefijnb2@gmail.com>
2024-02-29 11:43:24 +0100

    Remove comment

--- aplib.net-demo/Assets/Scripts/GameManager.cs
+++ aplib.net-demo/Assets/Scripts/GameManager.cs
@@ -16,7 +16,6 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        // Setup singleton
         if (_instance != null && _instance != this)
             Destroy(gameObject);
         else
