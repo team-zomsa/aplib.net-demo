05f19964e5f3f3ebf03ff1c98c39cd9b5e53d96b
Quake <QuakeEye@users.noreply.GitHub.com>
2024-03-06 18:07:52 +0100

    chore: clean up final code

--- aplib.net-demo/Assets/Scripts/InputManager.cs
+++ aplib.net-demo/Assets/Scripts/InputManager.cs
@@ -11,7 +11,6 @@ public class InputManager : MonoBehaviour
     private PlayerInput.UIActions _uiActions;
 
     private Vector2 _horizontalInput;
-    private Vector2 _mouseInput;
 
     public static InputManager Instance { get; private set; }
 
@@ -32,7 +31,6 @@ public class InputManager : MonoBehaviour
 
         _playerActions.Move.performed += inputContext => _horizontalInput = inputContext.ReadValue<Vector2>();
         _playerActions.Jump.performed += _ => _movement.OnJumpPressed();
-        _playerActions.Look.performed += inputContext => _mouseInput = inputContext.ReadValue<Vector2>();
         _uiActions.ShowMouse.performed += _ => _mouseLock.OnShowMousePressed();
         _uiActions.Click.performed += _ => _mouseLock.OnLeftMousePressed();
     }
@@ -40,26 +38,14 @@ public class InputManager : MonoBehaviour
     /// <summary>
     /// Get the change in mouse position since the last frame.
     /// </summary>
-    public Vector2 GetMouseDelta()
-    {
-        return _playerActions.Look.ReadValue<Vector2>();
-    }
+    public Vector2 GetMouseDelta() => _playerActions.Look.ReadValue<Vector2>();
 
     /// <summary>
     /// Pass the input to the movement and mouse look scripts.
     /// </summary>
-    private void Update()
-    {
-        _movement.ReceiveHorizontalInput(_horizontalInput);
-    }
+    private void Update() => _movement.ReceiveHorizontalInput(_horizontalInput);
 
-    private void OnEnable()
-    {
-        _input.Enable();
-    }
+    private void OnEnable() => _input.Enable();
 
-    private void OnDisable()
-    {
-        _input.Disable();
-    }
+    private void OnDisable() => _input.Disable();
 }
