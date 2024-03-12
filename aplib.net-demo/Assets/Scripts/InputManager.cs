e02aaba8d372d75580e136701fcafab99c49f8b0
Quake <74355598+QuakeEye@users.noreply.github.com>
2024-03-12 16:06:10 +0100

    feat: add camera manager (#10)
    * feat: add scene for branch
    
    * feat: install cinemachine
    
    * feat: add Cinemachine virtual camera
    
    * feat: add cinemachine setup
    
    * feat: add initial camera manager
    
    * fix: remove NullReferenceException caused by Cinemachine extension
    
    * feat: move references to camera manager
    
    * chore: clean up final code
    
    * chore: implement some requested styling changes
    
    ---------
    
    Co-authored-by: Quake <QuakeEye@users.noreply.GitHub.com>

--- aplib.net-demo/Assets/Scripts/InputManager.cs
+++ aplib.net-demo/Assets/Scripts/InputManager.cs
@@ -4,14 +4,13 @@ using UnityEngine.InputSystem;
 public class InputManager : MonoBehaviour
 {
     [SerializeField] private Movement _movement;
-    [SerializeField] private MouseLook _mouseLook;
+    [SerializeField] private MouseLock _mouseLock;
 
     private PlayerInput _input;
     private PlayerInput.PlayerActions _playerActions;
     private PlayerInput.UIActions _uiActions;
 
     private Vector2 _horizontalInput;
-    private Vector2 _mouseInput;
 
     public static InputManager Instance { get; private set; }
 
@@ -32,28 +31,21 @@ public class InputManager : MonoBehaviour
 
         _playerActions.Move.performed += inputContext => _horizontalInput = inputContext.ReadValue<Vector2>();
         _playerActions.Jump.performed += _ => _movement.OnJumpPressed();
-        _playerActions.LookX.performed += inputContext => _mouseInput.x = inputContext.ReadValue<float>();  
-        _playerActions.LookY.performed += inputContext => _mouseInput.y = inputContext.ReadValue<float>();
-        _uiActions.ShowMouse.performed += _ => _mouseLook.OnShowMousePressed();
-        _uiActions.Click.performed += _ => _mouseLook.OnLeftMousePressed();
+        _uiActions.ShowMouse.performed += _ => _mouseLock.OnShowMousePressed();
+        _uiActions.Click.performed += _ => _mouseLock.OnLeftMousePressed();
     }
 
     /// <summary>
-    /// Pass the input to the movement and mouse look scripts.
+    /// Get the change in mouse position since the last frame.
     /// </summary>
-    private void Update()
-    {
-        _movement.ReceiveHorizontalInput(_horizontalInput);
-        _mouseLook.ReceiveMouseInput(_mouseInput);
-    }
+    public Vector2 GetMouseDelta() => _playerActions.Look.ReadValue<Vector2>();
 
-    private void OnEnable()
-    {
-        _input.Enable();
-    }
+    /// <summary>
+    /// Pass the input to the movement script.
+    /// </summary>
+    private void Update() => _movement.ReceiveHorizontalInput(_horizontalInput);
 
-    private void OnDisable()
-    {
-        _input.Disable();
-    }
+    private void OnEnable() => _input.Enable();
+
+    private void OnDisable() => _input.Disable();
 }
