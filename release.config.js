785a521f2f44fa6fefc47f87e2810cf484d7d369
Quake <QuakeEye@users.noreply.GitHub.com>
2024-03-13 13:46:52 +0100

    chore: remove comments

--- release.config.js
+++ release.config.js
@@ -5,17 +5,6 @@ module.exports = {
       "@semantic-release/github",
       [
         "@semantic-release/exec"
-        
-        
-        // Theoretical scenario for possible extension in which the project
-        //  has its version set and is published respectively with the following
-        //  commands:
-        
-        // ,
-        // {
-        //   "prepareCmd": "set-version ${nextRelease.version}",
-          // "publishCmd": "publish-package"
-        // }
       ]
     ],
     "branches": [
