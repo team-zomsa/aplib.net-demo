825a857e57a2d412a8290471039b134b81ae112e
Joen van de Vorle <j.j.vandevorle@students.uu.nl>
2024-03-14 11:18:52 +0100

    feat: add respawn on R

---
+++ aplib.net-demo/Assets/Scripts/Player/ResetRigidbody.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+/// <summary>
+/// Script to reset the position and velocity of a Rigidbody to its initial state.
+/// Requires the object to have a Rigidbody component.
+/// </summary>
+public class ResetRigidbody : MonoBehaviour
+{
+    Rigidbody _rigidbodyToRespawn;
+    private Vector3 _respawnPoint;
+
+    /// <summary>
+    /// Get the Rigidbody component and store the initial position.
+    /// </summary>
+    private void Awake()
+    {
+        _rigidbodyToRespawn = transform.GetComponent<Rigidbody>();
+        _respawnPoint = _rigidbodyToRespawn.position;
+    }
+
+    /// <summary>
+    /// Reset the position and velocity of the Rigidbody 
+    /// </summary>
+    public void ResetObject()
+    {
+        _rigidbodyToRespawn.position = _respawnPoint;
+        _rigidbodyToRespawn.velocity = Vector3.zero;
+    }
+}
