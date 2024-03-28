00d67b61a6adcaaa722eed00cc79b8b43f48a672
Thijs Boerefijn <tboefijnb2@gmail.com>
2024-03-28 11:52:23 +0100

    feat: use index instead of if ... else ...

--- aplib.net-demo/Assets/Scripts/Tiles/Corner.cs
+++ aplib.net-demo/Assets/Scripts/Tiles/Corner.cs
@@ -9,27 +9,18 @@ namespace Assets.Scripts.Tiles
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="Corner"/> class.
-        /// The default is a bottom-right corner.
+        /// The default is a top-right corner.
         /// </summary>
         /// <param name="rotate">The amount of times to rotate the tile.</param>
         public Corner(int rotate = 0)
         {
-            if (rotate == 0)
-            {
-                _allowedDirections = new List<bool> { false, true, true, false };
-            }
-            else if (rotate == 1)
-            {
-                _allowedDirections = new List<bool> { false, false, true, true };
-            }
-            else if (rotate == 2)
-            {
-                _allowedDirections = new List<bool> { true, false, false, true };
-            }
-            else
-            {
-                _allowedDirections = new List<bool> { true, true, false, false };
-            }
+            _allowedDirections = new List<bool> { false, false, false, false };
+
+            int index = rotate % 4;
+            int nextIndex = (index + 1) % 4;
+
+            _allowedDirections[index] = true;
+            _allowedDirections[nextIndex] = true;
         }
     }
 }
