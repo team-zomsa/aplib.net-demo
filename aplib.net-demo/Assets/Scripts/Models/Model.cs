8e2c19f3f7542129e8b30cb623438498510557e5
Thijs Boerefijn <tboefijnb2@gmail.com>
2024-03-28 16:14:58 +0100

    feat: add model

--- aplib.net-demo/Assets/Scripts/Models/Model.cs
+++ aplib.net-demo/Assets/Scripts/Models/Model.cs
@@ -1,13 +1,26 @@
+using Assets.Scripts.Tiles;
+using Assets.Scripts.WFC;
 using System;
 
 namespace Assets.Scripts.Models
 {
+
     public class Model
     {
-        private readonly Func<bool> _func;
+        private readonly Func<Tile, int, int, Grid, bool> _func;
+        private readonly Tile _tile;
+        private readonly int _x;
+        private readonly int _y;
+        private readonly Grid _grid;
 
-        public Model(Func<bool> func) => _func = func;
+        public Model(Tile tile, int x, int y, Func<Tile, int, int, Grid, bool> func)
+        {
+            _tile = tile;
+            _x = x;
+            _y = y;
+            _func = func;
+        }
 
-        public bool Feasable() => _func();
+        public bool Feasable() => _func(_tile, _x, _y, _grid);
     }
 }
