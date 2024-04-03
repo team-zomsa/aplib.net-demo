4689b6f9ab195bf0f5bbee1f526d06e35ab5f60f
Joen van de Vorle <j.j.vandevorle@students.uu.nl>
2024-04-03 14:42:40 +0200

    feat: add basic melee weapon
    sometimes causes nullreference during fire inputaction?

--- aplib.net-demo/Assets/Scripts/Player/Weapons/MeleeWeapon.cs
+++ aplib.net-demo/Assets/Scripts/Player/Weapons/MeleeWeapon.cs
@@ -2,8 +2,34 @@ using UnityEngine;
 
 public class MeleeWeapon : Weapon
 {
+    [SerializeField] private float _damage = 25;
+    [SerializeField] private float _range = 3;
+
+    private Transform _playerTransform;
+
+    private void Start()
+    {
+        _playerTransform = transform.parent;
+    }
+
+    /// <summary>
+    /// Shoot a ray from the weapon's position in the direction it is facing.
+    /// Look for an entity with an Enemy tag and deal damage to it.
+    /// </summary>
     public override void UseWeapon()
     {
-        Debug.Log("Swinging sword!");
+        RaycastHit hit;
+        if (Physics.Raycast(_playerTransform.position, transform.forward, out hit, _range))
+        {
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                hit.collider.GetComponent<BasicEnemy>().TakeDamage(_damage);
+                Debug.Log("Swinging sword at " + hit.collider.name);
+            }
+        }
+        else 
+        {
+            Debug.Log("Swinging sword at nothing");
+        }
     }
 }
