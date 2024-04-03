4f8c8b5063d619aa5807eecb14ac72d2c731da05
Joen van de Vorle <j.j.vandevorle@students.uu.nl>
2024-04-03 15:17:06 +0200

    chore: add comments and separate some logic

---
+++ aplib.net-demo/Assets/Scripts/Enemy/BasicEnemy.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+/// <summary>
+/// Very basic enemy class.
+/// </summary>
+public class BasicEnemy : MonoBehaviour
+{
+    [SerializeField] protected float _maxHealth = 100;
+    public float Health { get; protected set; }
+
+    /// <summary>
+    /// Initializes the enemy's health to the maximum value.
+    /// </summary>
+    protected virtual void Awake()
+    {
+        Health = _maxHealth;
+    }
+
+    /// <summary>
+    /// Reduces the enemy's health by the specified amount.
+    /// If the health reaches zero, the enemy dies.
+    /// </summary>
+    /// <param name="damage"></param>
+    public virtual void TakeDamage(float damage)
+    {
+        Health -= damage;
+        if (Health <= 0)
+        {
+            Die();
+        }
+    }
+
+    /// <summary>
+    /// Kills the enemy.
+    /// </summary>
+    protected virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+}
