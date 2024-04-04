f37ec2cdef4ed3ac553de28bdb424c3eb1cc4de9
Joen van de Vorle <j.j.vandevorle@students.uu.nl>
2024-04-04 15:57:51 +0200

    fix: resolve PR feedback

--- aplib.net-demo/Assets/Scripts/Camera/CameraManager.cs
+++ aplib.net-demo/Assets/Scripts/Camera/CameraManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] public Transform PlayerVisTransform;
+    public Transform PlayerTransform;
 
     public static CameraManager Instance { get; private set; }
 
@@ -18,5 +20,7 @@ public class CameraManager : MonoBehaviour
         else
             Instance = this;
         DontDestroyOnLoad(gameObject);
+
+        PlayerTransform = PlayerVisTransform.parent;
     }
 }
