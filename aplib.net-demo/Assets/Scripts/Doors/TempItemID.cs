77fa82469549d356127cecb2fc89d6811c832d64
Pepijn Lamers <p.p.lamers@students.uu.nl>
2024-04-11 15:44:05 +0200

    Changed door texture

--- aplib.net-demo/Assets/Scripts/Doors/TempItemID.cs
+++ aplib.net-demo/Assets/Scripts/Doors/TempItemID.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
-/// Temporary class that gives an ID to the player
+/// Temporary class that gives an ID to the player, change the ID in the editor of the player object
 /// </summary>
 public class TempItemID : MonoBehaviour
 {
