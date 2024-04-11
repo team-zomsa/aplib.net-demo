7aa6d0b2c0fc5183ef1e0a25915bd2945e5b7d6f
Pepijn Lamers <p.p.lamers@students.uu.nl>
2024-04-11 15:35:13 +0200

    Added tempitemID class, added door logic, added summaries, added door prefab

--- aplib.net-demo/Assets/Scripts/Doors/DoorCollision.cs
+++ aplib.net-demo/Assets/Scripts/Doors/DoorCollision.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// This class handles collisions with the player and makes sure that the parent object (the door) disappears/opens when the players is in range and the prerequisites are met (the right key, or in this case the right player ID)
+/// Set the ID in the editor for the collider
+/// </summary>
 public class DoorCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int doorID;
+    [SerializeField] private GameObject parent;
+    /// <summary>
+    /// Checks if the player has the right ID, and destroys the door if true
+    /// </summary>
+    /// <param name="other">the object that collides with the collider</param>
+    private void OnTriggerEnter(Collider collidingObject)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //Delete door
+        if (collidingObject.gameObject.tag == "Player" && collidingObject.gameObject.GetComponent<TempItemID>().itemID == doorID)
+        {
+            Destroy(parent);
+        }
     }
 }
---
+++ aplib.net-demo/Assets/Scripts/Doors/TempItemID.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Temporary class that gives an ID to the player
+/// </summary>
+public class TempItemID : MonoBehaviour
+{
+    public int itemID;
+}
