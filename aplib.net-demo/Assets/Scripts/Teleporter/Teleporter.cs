7e44924ac6528cda2538bdb82b5b0f3dca2b461d
SilasPeters <silaspeters03@gmail.com>
2024-04-12 22:20:08 +0200

    feat: finish teleporter prefab

--- aplib.net-demo/Assets/Scripts/Teleporter/Teleporter.cs
+++ aplib.net-demo/Assets/Scripts/Teleporter/Teleporter.cs
@@ -3,26 +3,80 @@ using UnityEngine;
 
 namespace Teleporter
 {
+    /// <summary>
+    /// An isolated teleporter set up to teleport the player to another target teleporter.
+    /// Optionally, this target teleporter targets this teleporter back, coupling both.
+    /// </summary>
     public class Teleporter : MonoBehaviour
     {
-        public float teleportDelay;
-        
+        /// <summary>
+        /// The duration of the delay before the player gets teleported.
+        /// </summary>
+        [Header("Parameters")]
+        public float teleportWindUpTime = 2f;
+
+        /// <summary>
+        /// An offset from this teleporter's position, on which the player will be teleported to when teleporting to this
+        /// teleporter. That's a lot of teleporting in one sentence.
+        /// </summary>
+        public Vector3 landingPointOffset = new(0, 1, 0);
+
+        /// <summary>
+        /// A reference to the FX parent gameobject, containing all special effects of the teleporter to be
+        /// activated during wind up.
+        /// </summary>
+        [Header("References")]
         public GameObject FX;
-        public Teleporter targetTeleporter;
-        public Vector3 landingPointOffset; // TODO draw gizmo en defaut value? En comment dit
-
-        protected Vector3 _landingPoint => transform.position + landingPointOffset;
         
-        private Coroutine _waitThenTeleportCoroutine;
-        private bool _shouldIgnoreNextPlayerEntry;
+        /// <summary>
+        /// The teleporter to which the player must be teleported. This is a one-directional link. To be bidirectional,
+        /// the target teleported must target this teleporter as back.
+        /// </summary>
+        public Teleporter targetTeleporter;
 
+        /// <summary>
+        /// The absolute position to which the player will be teleported to when teleporting to this teleporter.
+        /// </summary>
+        protected Vector3 _landingPoint => transform.position + landingPointOffset;
+
+        /// <summary>
+        /// A simple reference to the player's transform, for performance reasons.
+        /// </summary>
+        protected Transform _playerTransform;
+
+        /// <summary>
+        /// A reference to the coroutine running while teleporting. Stored, such that the teleporting can be cancelled.
+        /// </summary>
+        private Coroutine _waitThenTeleportCoroutine;
+        
+        /// <summary>
+        /// When the player gets teleported to this teleporter, you do not want logic to trigger which would normally
+        /// be triggered when the player walks into the teleporter. For example, the player should not trigger this
+        /// teleporter when being teleported to this teleporter.
+        /// </summary>
+        private bool _shouldIgnoreNextPlayerEntryTrigger;
+
+        /// <summary>
+        /// Get global GameObjet references once.
+        /// </summary>
+        private void Start()
+        {
+            _playerTransform = GameObject.FindWithTag("Player").transform;
+        }
+
+        /// <summary>
+        /// When the player walks in, the teleporter should start winding up and teleport.
+        /// </summary>
+        /// <param name="other">The object which should be the player</param>
         private void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag("Player")) return; // Only trigger for player
-            if (_shouldIgnoreNextPlayerEntry)
+            
+            if (_shouldIgnoreNextPlayerEntryTrigger) 
             {
-                Debug.Log("Ignored...", this);
-                _shouldIgnoreNextPlayerEntry = false;
+                // Do not teleport when the player triggers this because of being teleported to this teleporter
+                _shouldIgnoreNextPlayerEntryTrigger = false;
+                Debug.Log("Ignore!");
                 return;
             }
 
@@ -34,9 +88,9 @@ namespace Teleporter
 
         /// <summary>
         /// Triggered when the player exits prematurely to cancel the teleport, AND when the teleport was successful
-        /// (as the player thus also exits it)
+        /// (as the player thus also exits it).
         /// </summary>
-        /// <param name="other">TODO</param>
+        /// <param name="other">The object which should be the player</param>
         private void OnTriggerExit(Collider other)
         {
             if (!other.CompareTag("Player")) return; // Only trigger for player
@@ -44,28 +98,41 @@ namespace Teleporter
             Debug.Log("Exit!");
 
             FX.SetActive(false);
-            StopCoroutine(_waitThenTeleportCoroutine); // Stop coroutine prematurely if still active
+            if (_waitThenTeleportCoroutine != null) // Can be null when player gets teleported to this teleporter
+                StopCoroutine(_waitThenTeleportCoroutine); // Stop coroutine prematurely when player exits during wind up time
         }
 
+        /// <summary>
+        /// Waits for <see cref="teleportWindUpTime"/>, then starts the teleport. This allows the FX to play.
+        /// </summary>
+        /// <returns>IEnumerator representing coroutine</returns>
         private IEnumerator WaitThenTeleport()
         {
-            yield return new WaitForSeconds(teleportDelay);
+            yield return new WaitForSeconds(teleportWindUpTime);
             
             // Teleport the player
-            // TODO finding the player is repetitive code. Remove it
-            Transform playerTransform = GameObject.FindWithTag("Player").transform;
-            targetTeleporter.TeleportReceive(playerTransform);
+            targetTeleporter.TeleportReceive(_playerTransform);
         }
 
+        /// <summary>
+        /// Teleport a <see cref="Transform"/> (normally the player) towards this teleporter.
+        /// </summary>
+        /// <param name="objectTransform">The transform to teleport</param>
         public void TeleportReceive(Transform objectTransform)
         {
             objectTransform.position = _landingPoint;
-            _shouldIgnoreNextPlayerEntry = true;
+            // Prevent this action from triggering logic which are intended for when the player walks in.
+            _shouldIgnoreNextPlayerEntryTrigger = true;
         }
 
-        private void OnDrawGizmos()
+        /// <summary>
+        /// Draw visual aid in the unity editor, when this teleporter is selected.
+        /// </summary>
+        private void OnDrawGizmosSelected()
         {
-            Gizmos.DrawSphere(_landingPoint, 0.3f);
+            Gizmos.DrawWireSphere(_landingPoint, 0.2f); // Indicate own landingPoint
+            Gizmos.DrawSphere(targetTeleporter._landingPoint, 0.3f); // Indicate target landingPoint
+            Gizmos.DrawLine(_landingPoint, targetTeleporter._landingPoint); // Indicate which portals is targeted
         }
     }
 }
--- aplib.net-demo/Assets/Scripts/Teleporter/TeleporterFX.cs
+++ aplib.net-demo/Assets/Scripts/Teleporter/TeleporterFX.cs
@@ -4,17 +4,51 @@ namespace Teleporter
 {
     public class TeleporterFX : MonoBehaviour
     {
+        /// <summary>
+        /// The momentum gained over time once triggered.
+        /// </summary>
+        [Header("Parameters")]
         [Range(0, .04f)] public float acceleration;
+
+        /// <summary>
+        /// The width gained over time once triggered.
+        /// </summary>
         [Range(0, .4f)] public float widthGain;
+
+        /// <summary>
+        /// Whether the FX should rotate clockwise to the right or left. True if right.
+        /// </summary>
         public bool rightHandedRotation;
 
+        /// <summary>
+        /// The 'FX' which is manipulated by this script.
+        /// </summary>
         protected Transform _plane;
+
+        /// <summary>
+        /// The starting scale as configured in the editor.
+        /// </summary>
         protected Vector3 _originalPlaneScale;
+
+        /// <summary>
+        /// The current acceleration, diverging from the original <see cref="acceleration"/>.
+        /// </summary>
         protected float _currentAcceleration;
 
+        /// <summary>
+        /// The current speed, diverging from 0.
+        /// </summary>
         protected float _currentSpeed;
+
+        /// <summary>
+        /// The current width, diverging from the <see cref="_originalPlaneScale"/>'s width.
+        /// </summary>
         protected float _currentWidth;
 
+        /// <summary>
+        /// Determine all original values of the FX, as configured in the editor.
+        /// These values will change over time, but the original must be remembered.
+        /// </summary>
         private void Start()
         {
             acceleration *= rightHandedRotation ? 1f : -1f;
@@ -25,7 +59,7 @@ namespace Teleporter
         }
 
         /// <summary>
-        /// Reset to initial state
+        /// (Re)set to initial state
         /// </summary>
         private void OnEnable()
         {
@@ -34,6 +68,9 @@ namespace Teleporter
             _currentWidth = 0;
         }
 
+        /// <summary>
+        /// Animate the FX.
+        /// </summary>
         private void Update()
         {
             // Rotate over time
