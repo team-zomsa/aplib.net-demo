17a6fce55b673fe87774c9c7f7952ce5e47cd471
wingbladedota <56514789+wingbladedota@users.noreply.github.com>
2024-04-15 14:37:10 +0200

    fix: extra whiteline
    Co-authored-by: Luka <51298607+LuukBerkers@users.noreply.github.com>

--- aplib.net-demo/Assets/Scripts/Doors/TempItemID.cs
+++ aplib.net-demo/Assets/Scripts/Doors/TempItemID.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 /// <summary>
 /// Temporary class that gives an ID to the player, change the ID in the editor of the player object
 /// </summary>
