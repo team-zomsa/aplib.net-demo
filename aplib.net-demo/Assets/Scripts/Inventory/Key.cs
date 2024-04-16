164b96e901584cccee9714e9615aa656df9aa101
wingbladedota <56514789+wingbladedota@users.noreply.github.com>
2024-04-16 14:45:05 +0200

    feat: todos
    add todos to eky class

--- aplib.net-demo/Assets/Scripts/Inventory/Key.cs
+++ aplib.net-demo/Assets/Scripts/Inventory/Key.cs
@@ -1,5 +1,12 @@
 public class Key : Item
 {
+    /*
+    todo: flesh out the pickup function with picking up keys
+    change the door collision to check the list in the inventory (make inventory static?)
+    find key image (see through?) 
+    create key display on top left of screen with number
+    add number to doors
+     */
     public int id;
     private void Start()
     {
