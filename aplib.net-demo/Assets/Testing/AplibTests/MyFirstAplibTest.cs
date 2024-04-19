94fa6f956b4d07b2403283a34bb2beea353c6586
Joachim Dekker <jdekker3008@gmail.com>
2024-04-19 21:10:20 +0200

    feat!: Remove Maze Solver and edit mode tests

--- aplib.net-demo/Assets/Testing/AplibTests/MyFirstAplibTest.cs
+++ aplib.net-demo/Assets/Testing/AplibTests/MyFirstAplibTest.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using NUnit.Framework;
-using UnityEngine;
-using UnityEngine.TestTools;
 using Aplib.Core;
 using Aplib.Core.Belief;
 using Aplib.Core.Desire;
@@ -9,76 +5,87 @@ using Aplib.Core.Desire.Goals;
 using Aplib.Core.Intent.Actions;
 using Aplib.Core.Intent.Tactics;
 using Aplib.Integrations.Unity;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
 
-public class MyFirstBeliefSet : BeliefSet
+namespace Tests.AplibTests
 {
-    /// <summary>
-    /// The player object in the scene.
-    /// </summary>
-    public Belief<GameObject, GameObject> Player = new(GameObject.Find("Player"), x => x);
-
-    /// <summary>
-    /// The target position that the player needs to move towards.
-    /// </summary>
-    public Belief<Transform, Vector3> TargetPosition = new(GameObject.Find("Target").transform, x => x.position);
-}
-
-public class MyFirstAplibTest
-{
-    [SetUp]
-    public void SetUp()
+    public class MyFirstBeliefSet : BeliefSet
     {
-        Debug.Log("Starting test MyFirstAplibTest");
-        SceneManager.LoadScene("FirstTestScene");
+        /// <summary>
+        /// The player object in the scene.
+        /// </summary>
+        public readonly Belief<GameObject, GameObject> Player = new(reference: GameObject.Find("Player"), x => x);
+
+        /// <summary>
+        /// The target position that the player needs to move towards.
+        /// </summary>
+        public readonly Belief<Transform, Vector3> TargetPosition =
+            new(GameObject.Find("Target").transform, x => x.position);
     }
 
-    /// <summary>
-    /// A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use `yield return null;` to skip a frame.
-    /// </summary>
-    [UnityTest]
-    public IEnumerator PerformMyFirstAplibTest()
+    public class MyFirstAplibTest
     {
-        // Arrange
-        // Create a belief set for the agent.
-        MyFirstBeliefSet beliefSet = new();
+        /// <summary>
+        /// A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use `yield return null;` to skip a frame.
+        /// </summary>
+        [UnityTest]
+        public IEnumerator PerformMyFirstAplibTest()
+        {
+            // Arrange
+            // Create a belief set for the agent.
+            MyFirstBeliefSet beliefSet = new();
 
-        // Create an intent for the agent that moves the agent towards the target position.
-        Action<MyFirstBeliefSet> moveTowardsTargetAction = new(
-            effect: beliefSet =>
-            {
-                GameObject player = beliefSet.Player;
-                float playerSpeed = 10f;
-                Vector3 playerPosition = player.transform.position;
-                Vector3 targetPosition = beliefSet.TargetPosition;
-                player.transform.position = Vector3.MoveTowards(playerPosition, targetPosition, playerSpeed * Time.deltaTime);
-            }
-        );
-        PrimitiveTactic<MyFirstBeliefSet> moveTowardsTargetTactic = new(action: moveTowardsTargetAction);
+            // Create an intent for the agent that moves the agent towards the target position.
+            Action<MyFirstBeliefSet> moveTowardsTargetAction = new(
+                beliefSet =>
+                {
+                    GameObject player = beliefSet.Player;
+                    const float playerSpeed = 10f;
+                    Vector3 playerPosition = player.transform.position;
+                    Vector3 targetPosition = beliefSet.TargetPosition;
+                    player.transform.position = Vector3.MoveTowards(playerPosition,
+                        targetPosition,
+                        maxDistanceDelta: playerSpeed * Time.deltaTime
+                    );
+                }
+            );
+            PrimitiveTactic<MyFirstBeliefSet> moveTowardsTargetTactic = new(moveTowardsTargetAction);
 
-        // Create a desire for the agent to reach the target position.
-        Goal<MyFirstBeliefSet> reachTargetGoal = new(
-            tactic: moveTowardsTargetTactic,
-            predicate: beliefSet =>
-            {
-                GameObject player = beliefSet.Player;
-                Vector3 playerPosition = player.transform.position;
-                Vector3 targetPosition = beliefSet.TargetPosition;
-                return Vector3.Distance(playerPosition, targetPosition) < 0.1f;
-            }
-        );
-        PrimitiveGoalStructure<MyFirstBeliefSet> reachTargetGoalStructure = new(goal: reachTargetGoal);
-        RepeatGoalStructure<MyFirstBeliefSet> repeat = new(goalStructure: reachTargetGoalStructure);
-        DesireSet<MyFirstBeliefSet> desireSet = new(mainGoal: repeat);
+            // Create a desire for the agent to reach the target position.
+            Goal<MyFirstBeliefSet> reachTargetGoal = new(
+                moveTowardsTargetTactic,
+                beliefSet =>
+                {
+                    GameObject player = beliefSet.Player;
+                    Vector3 playerPosition = player.transform.position;
+                    Vector3 targetPosition = beliefSet.TargetPosition;
+                    return Vector3.Distance(playerPosition, targetPosition) < 0.1f;
+                }
+            );
+            PrimitiveGoalStructure<MyFirstBeliefSet> reachTargetGoalStructure = new(reachTargetGoal);
+            RepeatGoalStructure<MyFirstBeliefSet> repeat = new(reachTargetGoalStructure);
+            DesireSet<MyFirstBeliefSet> desireSet = new(repeat);
 
-        // Setup the agent with the belief set and desire set and initialize the test runner.
-        BdiAgent<MyFirstBeliefSet> agent = new(beliefSet, desireSet);
-        AplibRunner testRunner = new(agent);
+            // Setup the agent with the belief set and desire set and initialize the test runner.
+            BdiAgent<MyFirstBeliefSet> agent = new(beliefSet, desireSet);
+            AplibRunner testRunner = new(agent);
 
-        // Act
-        yield return testRunner.Test();
+            // Act
+            yield return testRunner.Test();
 
-        // Assert
-        Assert.AreEqual(agent.Status, CompletionStatus.Success);
+            // Assert
+            Assert.AreEqual(CompletionStatus.Success, agent.Status);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Debug.Log("Starting test MyFirstAplibTest");
+            SceneManager.LoadScene("FirstTestScene");
+        }
     }
 }
