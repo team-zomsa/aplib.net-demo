d8a822657055ba7db3529f396c1a8633dd5b3988
Joen <90771356+JoenvandeVorle@users.noreply.github.com>
2024-04-21 20:43:17 +0200

    feat: add melee weapon (#26)
    * feat: add very basic weapon and enemy
    
    * feat: add basic melee weapon
    
    sometimes causes nullreference during fire inputaction?
    
    * fix: no more NullReferenceExceptions
    
    * chore: add tags to objects in scene
    
    * chore: add comments and separate some logic
    
    * feat: use collision zone instead of raycast
    
    * fix: resolve PR feedback
    
    * fix: forgot to save scene -_-
    
    * fix: resolved PR feedback
    
    * chore: merge main
    
    * feat: add very basic weapon and enemy
    
    * feat: add basic melee weapon
    
    sometimes causes nullreference during fire inputaction?
    
    * fix: no more NullReferenceExceptions
    
    * chore: add tags to objects in scene
    
    * chore: add comments and separate some logic
    
    * feat: use collision zone instead of raycast
    
    * fix: resolve PR feedback
    
    * fix: forgot to save scene -_-
    
    * fix: resolved PR feedback
    
    * chore: add using
    
    * chore: add comments and move some logic
    
    * chore: remove spelling mistake
    
    ---------
    
    Co-authored-by: Quake <QuakeEye@users.noreply.GitHub.com>

---
+++ aplib.net-demo/Assets/Scripts/Util/Area.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+/// <summary>
+/// The spawn area for the enemy. 
+/// The size is defined by the local scale of the transform.
+/// </summary>
+public class Area : MonoBehaviour
+{
+    /// <summary>
+    /// The bounds of the area.
+    /// </summary> 
+    [HideInInspector] private Bounds _bounds;
+    public Bounds Bounds => _bounds;
+
+    /// <summary>
+    /// Initialize the area bounds.
+    /// </summary>
+    private void Awake()
+    {
+        _bounds = new Bounds(transform.position, transform.localScale);
+    }
+
+    /// <summary>
+    /// Show the area in the editor.
+    /// </summary>
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Globals.s_LogicColor;
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
+    }
+}
