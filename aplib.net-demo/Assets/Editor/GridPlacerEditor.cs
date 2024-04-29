9432ced34fc09d034fd918a2269e57ddcf1dd35d
Thijs Boerefijn <51719415+Tboefijn@users.noreply.github.com>
2024-04-29 11:34:50 +0200

    fix: editor and room connections (#54)
    * fix: add door prefab in editor
    
    * fix: remove room connections outside of the grid
    
    * feat: add else if

--- aplib.net-demo/Assets/Editor/GridPlacerEditor.cs
+++ aplib.net-demo/Assets/Editor/GridPlacerEditor.cs
@@ -15,6 +15,7 @@ namespace Editors
             SerializedObject gridPlacerSettings = serializedObject;
 
             SerializedProperty roomObjects = gridPlacerSettings.FindProperty("_roomObjects");
+            SerializedProperty doorPrefab = gridPlacerSettings.FindProperty("_doorPrefab");
             SerializedProperty useSeed = gridPlacerSettings.FindProperty("_useSeed");
             SerializedProperty seed = gridPlacerSettings.FindProperty("_seed");
             SerializedProperty tileSizeX = gridPlacerSettings.FindProperty("_tileSizeX");
@@ -24,6 +25,7 @@ namespace Editors
             SerializedProperty amountOfRooms = gridPlacerSettings.FindProperty("_amountOfRooms");
 
             roomObjects.objectReferenceValue = EditorGUILayout.ObjectField("Room objects", roomObjects.objectReferenceValue, typeof(RoomObjects), false);
+            doorPrefab.objectReferenceValue = EditorGUILayout.ObjectField("Door prefab", doorPrefab.objectReferenceValue, typeof(GameObject), false);
             useSeed.boolValue = EditorGUILayout.Toggle("Use seed", useSeed.boolValue);
             if (useSeed.boolValue) seed.intValue = EditorGUILayout.IntField("Seed", seed.intValue);
             tileSizeX.intValue = EditorGUILayout.IntField("Tile size X", tileSizeX.intValue);
