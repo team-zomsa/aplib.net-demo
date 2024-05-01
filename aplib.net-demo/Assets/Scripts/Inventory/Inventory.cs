03e9097af3f431f82a3147c2129145608bfe8f5d
wingbladedota <56514789+wingbladedota@users.noreply.github.com>
2024-05-01 12:55:29 +0200

    Update aplib.net-demo/Assets/Scripts/Inventory/Inventory.cs
    Co-authored-by: Jens Steenmetz <35835627+JensSteenmetz@users.noreply.github.com>

--- aplib.net-demo/Assets/Scripts/Inventory/Inventory.cs
+++ aplib.net-demo/Assets/Scripts/Inventory/Inventory.cs
@@ -32,7 +32,7 @@ public class Inventory : MonoBehaviour
     /// Converts queue to list to check if there are any items with matching names. If there are it checks if they are stackable and adds uses. If they are not it does nothing. If there are not matching names it adds the item to the inventory.
     /// </summary>
     /// <param name="item">The item that is fed into the inventory</param>
-    /// <param name="uses">the amount of uses that are added upon pickup</param>
+    /// <param name="uses">The amount of uses that are added upon pickup.</param>
     public void PickUpItem(Item item, float uses = 1)
     {
         bool alreadyInInventory = false;
