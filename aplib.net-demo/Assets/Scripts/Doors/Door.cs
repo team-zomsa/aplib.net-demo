5c7a5b9bb428f3fabcb3d5b7db2707459493a655
wingbladedota <56514789+wingbladedota@users.noreply.github.com>
2024-05-02 11:52:20 +0200

    fix: suggestions
    Co-authored-by: Thijs Boerefijn <51719415+Tboefijn@users.noreply.github.com>
    Co-authored-by: Jens Steenmetz <35835627+JensSteenmetz@users.noreply.github.com>

--- aplib.net-demo/Assets/Scripts/Doors/Door.cs
+++ aplib.net-demo/Assets/Scripts/Doors/Door.cs
@@ -33,11 +33,13 @@ public class Door : MonoBehaviour
 
         //Once the keys and doors are given matching id's, we can use this
         /*
-                if (collidingObject.gameObject.CompareTag("Player") && collidingObject.GetComponent<KeyRing>().KeyQuery(this))
-                {
-                    Destroy(transform.parent.gameObject);
-                }*/
+          if (collidingObject.gameObject.CompareTag("Player") && collidingObject.GetComponent<KeyRing>().KeyQuery(this))
+            {
+              Destroy(transform.parent.gameObject);
+            }
+        */
     }
+    
     /// <summary>
     /// Matches the given key's ID with the door's ID, returns true if the same
     /// </summary>
