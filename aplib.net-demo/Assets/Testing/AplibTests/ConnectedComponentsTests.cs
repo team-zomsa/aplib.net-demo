42c871ed9b98b399564b5f0cfd3011da0742506f
SilasPeters <69711739+SilasPeters@users.noreply.github.com>
2024-05-04 23:25:42 +0200

    test: first draft

--- aplib.net-demo/Assets/Testing/AplibTests/ConnectedComponentsTests.cs
+++ aplib.net-demo/Assets/Testing/AplibTests/ConnectedComponentsTests.cs
@@ -36,6 +36,60 @@ namespace Tests.AplibTests
             SceneManager.LoadScene(_sceneName);
         }
 
+        /// <summary>
+        /// Before the tests can begin, we must generate Nev Mash Data such that path finding works. Furthermore,
+        /// Nav Mesh Data from different connected components must be linked through Nav Mesh Links.
+        /// </summary>
+        private void BakeNavMesh()
+        {
+            // Before baking the nav mesh, we must link nav mesh data from different connected components linked through
+            // teleporters, such that the agent knows how to use the teleporters.
+            // Gather one teleporter from every teleporter pair
+            // Teleporter.Teleporter[] allTeleporters = GameObject.Find("Teleporters").GetComponentsInChildren<Teleporter.Teleporter>();
+            // List<Teleporter.Teleporter> entryTeleporters = new();
+            // foreach (Teleporter.Teleporter teleporter in allTeleporters)
+            // {
+            //     if (entryTeleporters.Any(x => x.targetTeleporter == teleporter))
+            //         continue;
+            //     entryTeleporters.Add(teleporter);
+            // }
+            //
+            // List<OffMeshLink> offMeshLinks = entryTeleporters.Select(teleporter => new OffMeshLink
+            //     {
+            //         activated = true,
+            //         area = 0,          // Walkable
+            //         costOverride = -1, // Explicitly use the default value
+            //         biDirectional = true,
+            //         startTransform = new RectTransform { position = teleporter.LandingPoint },
+            //         endTransform = new RectTransform { position = teleporter.targetTeleporter.LandingPoint }
+            //     }).ToList();
+
+            // Now, we bake!
+            NavMeshBuildSettings buildSettings = NavMesh.GetSettingsByIndex(0);
+            NavMeshData navMeshData = NavMeshBuilder.BuildNavMeshData(buildSettings, collectNavMeshSources(),
+                new Bounds(Vector3.zero, new Vector3(300, 50, 300)), Vector3.zero, Quaternion.identity); // TODO make grid the bounds
+            NavMesh.AddNavMeshData(navMeshData);
+            // NavMeshBuilder.UpdateNavMeshData(navMeshData, buildSettings, collectNavMeshSources(),
+            //     new Bounds(Vector3.zero, new Vector3(300, 50, 300))); // TODO obsolete?
+
+            return;
+
+            List<NavMeshBuildSource> collectNavMeshSources()
+            {
+                GameObject environment = GameObject.Find("Environment");
+
+                return (from meshFilter in environment.GetComponentsInChildren<MeshFilter>()
+                        where meshFilter.gameObject.GetComponent<Door>() == null // Do not include doors in baking
+                        select new NavMeshBuildSource
+                        {
+                            shape = NavMeshBuildSourceShape.Mesh,
+                            sourceObject = meshFilter.sharedMesh,
+                            transform = meshFilter.gameObject.transform.localToWorldMatrix,
+                            area = 0,
+                        }).ToList();
+            }
+        }
+
 
         /// <summary>
         /// This test first collects a single cell from every connected component.
@@ -46,38 +100,16 @@ namespace Tests.AplibTests
         [UnityTest]
         public IEnumerator CanVisitEveryConnectedComponent()
         {
-            // Before the tests can begin, we must generate Nev Mash Data such that path finding works. Furthermore,
-            // Nav Mesh Data from different connected components must be linked through Nav Mesh Links.
-            NavMeshBuildSettings buildSettings = NavMesh.GetSettingsByIndex(0);
-            NavMeshData navMeshData = NavMeshBuilder.BuildNavMeshData(buildSettings, collectNavMeshSources(),
-                new Bounds(Vector3.zero, new Vector3(300, 50, 300)), Vector3.zero, Quaternion.identity); // TODO make grid the bounds
-            NavMesh.AddNavMeshData(navMeshData);
-            NavMeshBuilder.UpdateNavMeshData(navMeshData, buildSettings, collectNavMeshSources(),
-                new Bounds(Vector3.zero, new Vector3(300, 50, 300)));
-
-            List<NavMeshBuildSource> collectNavMeshSources()
-            {
-                GameObject environment = GameObject.Find("Environment");
-
-                List<NavMeshBuildSource> sources = environment.GetComponentsInChildren<MeshFilter>()
-                    .Select(meshFilter => new NavMeshBuildSource // TODO exclude modivied meshes?
-                    {
-                        shape = NavMeshBuildSourceShape.Mesh,
-                        sourceObject = meshFilter.sharedMesh,
-                        transform = meshFilter.gameObject.transform.localToWorldMatrix,
-                        area = 0,
-                    }).ToList();
-
-                return sources.ToList();
-            }
+            BakeNavMesh();
 
             // Arrange
             ConnectedComponentsBeliefSet rootBeliefSet = new();
             GridPlacer gridPlacer = GameObject.Find("Grid").GetComponent<GridPlacer>();
 
             // Arrange ==> Level information
+            Vector3 centreOfCellHeightOffest = Vector3.up * 1.7f;
             Vector3[] cellsToVisit = gridPlacer.Grid.DetermineConnectedComponents()
-                .Select(cells => gridPlacer.CentreOfCell(cells.First())).ToArray();
+                .Select(cells => gridPlacer.CentreOfCell(cells.First()) + centreOfCellHeightOffest).ToArray();
             Vector3[] teleporterPositions = GameObject.Find("Teleporters")
                 .GetComponentsInChildren<Teleporter.Teleporter>()
                 .Select(x => x.LandingPoint).ToArray();
@@ -103,15 +135,15 @@ namespace Tests.AplibTests
                 approachCurrentCellTactic);
 
             Goal<ConnectedComponentsBeliefSet> approachCurrentCellGoal = new(waitForTeleportOrApproachCurrentCellTactic,
-                beliefSet => (currentCellPosition() - ((Transform)beliefSet.PlayerTransform).position).magnitude < 0.2f);
+                beliefSet => (currentCellPosition() - ((Transform)beliefSet.PlayerTransform).position).magnitude < 1.5f);
             PrimitiveGoalStructure<ConnectedComponentsBeliefSet> approachCurrentCellGoalStructure = new(approachCurrentCellGoal);
             RepeatGoalStructure<ConnectedComponentsBeliefSet> visitCurrentCellGoalStructure = new(approachCurrentCellGoalStructure);
 
             // Arrange ==> GoalStructure: Target the next cell until every cell has been targeted
             Action<ConnectedComponentsBeliefSet> targetNextCellAction = new(effect: _ =>
             {
+                Debug.Log($"Reached cell at {currentCellPosition()}");
                 currentCellToVisitIndex++;
-                Debug.Log($"Targeting next cell at {currentCellPosition()}");
             });
 
             PrimitiveTactic<ConnectedComponentsBeliefSet> targetNextCellTactic = new(targetNextCellAction);
