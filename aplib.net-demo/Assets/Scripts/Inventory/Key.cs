a9f611454d6b4e803ec94b2a88db928068f9dbee
Tboefijn <tboefijnb2@gmail.com>
2024-05-04 15:26:27 +0200

    feat: remove Key constructor

--- aplib.net-demo/Assets/Scripts/Inventory/Key.cs
+++ aplib.net-demo/Assets/Scripts/Inventory/Key.cs
@@ -3,14 +3,8 @@ public class Key : Item
     /// <summary>
     /// The key ID, to check which door it can open.
     /// </summary>
-    public int Id { get; private set; }
-    
-    /// <summary>
-    /// Initializes a key with a given ID.
-    /// </summary>
-    /// <param name="id">The ID the key is given.</param>
-    public Key(int id) => Id = id;
-    
+    public int Id;
+
     private void Start()
     {
         stackable = false;
