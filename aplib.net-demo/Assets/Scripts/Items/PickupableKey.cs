62fa21f96a0a465203a16299e0d487c0217b61ec
Tboefijn <tboefijnb2@gmail.com>
2024-05-16 20:06:19 +0200

    feat: reduce calling of expensive method

--- aplib.net-demo/Assets/Scripts/Items/PickupableKey.cs
+++ aplib.net-demo/Assets/Scripts/Items/PickupableKey.cs
@@ -8,23 +8,24 @@ namespace Assets.Scripts.Items
     [RequireComponent(typeof(Key))]
     public class PickupableKey : MonoBehaviour
     {
+        private KeyRing _keyRing;
+
+        public void Start()
+        {
+            GameObject inventoryObject = GameObject.Find("KeyRingObject");
+            _keyRing = inventoryObject.GetComponent<KeyRing>();
+
+            if (!_keyRing) throw new UnityException("Key ring not found!");
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag("Player"))
                 return;
 
-            GameObject inventoryObject = GameObject.Find("KeyRingObject");
-            KeyRing keyring = inventoryObject.GetComponent<KeyRing>();
-
-            if (!keyring)
-            {
-                Debug.LogWarning("Key ring not found!");
-                return;
-            }
-
             Key item = gameObject.GetComponent<Key>();
 
-            keyring.StoreKey(item);
+            _keyRing.StoreKey(item);
 
             Destroy(gameObject);
         }
