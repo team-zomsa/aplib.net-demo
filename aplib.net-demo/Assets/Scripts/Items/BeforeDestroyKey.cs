45a9899870e5b5372b1424e72ea57ee651eb69e8
Thijs Boerefijn <51719415+Tboefijn@users.noreply.github.com>
2024-05-28 23:01:34 +0200

    feat: spawn items (#80)
    * feat: remove colors
    
    * feat: use type alias for connected component
    
    * feat: spawn end item
    
    * feat: use separate class for instantiating prefabs
    
    * fix: null reference exception
    
    * feat: add method to get all cells that do not contain items
    
    * feat: add functions and variables for item placing
    
    * feat: implement item spawning
    
    * feat: update test scene
    
    * feat: update GridSystem scene
    
    * fix: don't throw null reference exception when picking up end item
    
    * fix: update test scene so the test still succeeds
    
    * feat: apply suggestion from code review
    
    Co-authored-by: Jens Steenmetz <35835627+JensSteenmetz@users.noreply.github.com>
    
    * feat: use center instead of centre
    
    * fix: correct writing of a word
    
    * feat: add begin of item spawning test
    
    * feat: fully implement working Aplib test
    
    * feat: use namespace
    
    * feat: make variables readonly
    
    * feat: add suggestion from code review
    
    Co-authored-by: Joachim Dekker <j.dekker9@students.uu.nl>
    
    * fix: spawning area
    
    * feat: apply suggestion from code review
    
    * fix: ConnectedComponentsTestScene.unity
    
    * feat: apply suggestions from code review
    
    Co-authored-by: Jens Steenmetz <35835627+JensSteenmetz@users.noreply.github.com>
    
    * fix: bugs introduced by applying suggestion
    
    * feat: update summary
    
    ---------
    
    Co-authored-by: Jens Steenmetz <35835627+JensSteenmetz@users.noreply.github.com>
    Co-authored-by: Joachim Dekker <j.dekker9@students.uu.nl>

---
+++ aplib.net-demo/Assets/Scripts/Items/BeforeDestroyKey.cs
@@ -0,0 +1,34 @@
+using Assets.Scripts.Doors;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Scripts.Items
+{
+    /// <summary>
+    /// Attach this class to make object pickupable.
+    /// </summary>
+    [RequireComponent(typeof(Key))]
+    public class BeforeDestroyKey : MonoBehaviour
+    {
+        private OffMeshLink _offMeshLink;
+
+        public void Awake()
+        {
+            Key key = gameObject.GetComponent<Key>();
+
+            if (!key) throw new UnityException("Key not found!");
+
+            GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Door");
+            GameObject door = gameObjects.FirstOrDefault(go => go.GetComponent<Door>().TryOpenDoor(key));
+
+            if (!door) throw new UnityException("Door not found!");
+
+            _offMeshLink = door.GetComponent<OffMeshLink>();
+
+            if (!_offMeshLink) throw new UnityException("OffMeshLink not found!");
+        }
+
+        private void OnDestroy() => _offMeshLink.activated = true;
+    }
+}
