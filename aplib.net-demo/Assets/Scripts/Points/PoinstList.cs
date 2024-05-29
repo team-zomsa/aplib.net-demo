6a516017764610950ec2dec599a1cd7e1eba53ea
Joen van de Vorle <90771356+JoenvandeVorle@users.noreply.github.com>
2024-05-29 20:01:54 +0200

    feat: add PointsManager, PointsList
    also moved managers to separate prefab folder

---
+++ aplib.net-demo/Assets/Scripts/Points/PoinstList.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+/// <summary>
+/// List of points for PointsManager, used to give a certain point value to certain game objects.
+/// </summary>
+[CreateAssetMenu(fileName = "PointsList", menuName = "ScriptableObjects/PointsList", order = 1)]
+public class PoinstList : ScriptableObject
+{
+
+}
---
+++ aplib.net-demo/Assets/Scripts/Points/PointsManager.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// Manages the player's points.
+/// </summary>
+public class PointsManager : MonoBehaviour
+{
+    /// <summary>
+    /// Event that is invoked when points are added.
+    /// </summary>
+    /// <param name="points">The current amount of points.</param>
+    public event Action<int> PointsAdded;
+
+    /// <summary>
+    /// The amount of points the player has.
+    /// </summary>
+    [field: SerializeField]
+    public int Points { get; private set; }
+
+    /// <summary>
+    /// The singleton instance of this class.
+    /// </summary>
+    public static PointsManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+        else
+            Instance = this;
+    }
+
+    /// <summary>
+    /// Add points to the player's total amount.
+    /// </summary>
+    /// <param name="points">The amount of points to add.</param>
+    public void AddPoints(int points)
+    {
+        Points += points;
+        PointsAdded?.Invoke(Points);
+    }
+}
