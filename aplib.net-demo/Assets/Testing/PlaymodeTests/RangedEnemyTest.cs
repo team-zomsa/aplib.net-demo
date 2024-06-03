1cbe02d9543c7391d53de648a530e37c4ab248fa
Joachim Dekker <jdekker3008@gmail.com>
2024-06-03 13:37:38 +0200

    fix: remove waiting for second

--- aplib.net-demo/Assets/Testing/PlaymodeTests/RangedEnemyTest.cs
+++ aplib.net-demo/Assets/Testing/PlaymodeTests/RangedEnemyTest.cs
@@ -4,39 +4,35 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
-
-namespace Testing.PlaymodeTests
+public class RangedEnemyTest
 {
-    public class RangedEnemyTest
+    [SetUp]
+    public void SetUp()
     {
-        [SetUp]
-        public void SetUp()
+        SceneManager.LoadScene("RangedEnemyTestScene");
+    }
+
+    [UnityTest]
+    public IEnumerator TestAmmo()
+    {
+        // Get the players crossbow
+        var player = GameObject.Find("Player");
+        Equipment crossbow = player.GetComponentInChildren<RangedWeapon>();
+        var ammoPouch = player.GetComponentInChildren<AmmoPouch>();
+
+        // Shoot the crossbow 10 times, but miss the shots
+        for (int i = 0; i < 10; i++)
         {
-            SceneManager.LoadScene("RangedEnemyTestScene");
-        }
-
-        [UnityTest]
-        public IEnumerator TestAmmo()
-        {
-            // Get the players crossbow
-            var player = GameObject.Find("Player");
-            Equipment crossbow = player.GetComponentInChildren<RangedWeapon>();
-            var ammoPouch = player.GetComponentInChildren<AmmoPouch>();
-
-            // Shoot the crossbow 10 times, but miss the shots
-            for (int i = 0; i < 10; i++)
-            {
-                crossbow.UseEquipment();
-                yield return new WaitForSeconds(0.1f);
-            }
-
-            // Try to use the crossbow one more time and check if it fails
             crossbow.UseEquipment();
-            yield return new WaitForSeconds(0.1f);
-
-            // Check if the player has no ammo left
-            bool hasAmmo = ammoPouch.TryUseAmmo();
-            Assert.IsFalse(hasAmmo);
+            yield return null;
         }
+
+        // Try to use the crossbow one more time and check if it fails
+        crossbow.UseEquipment();
+        yield return null;
+
+        // Check if the player has no ammo left
+        bool hasAmmo = ammoPouch.TryUseAmmo();
+        Assert.IsFalse(hasAmmo);
     }
 }
