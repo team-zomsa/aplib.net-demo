d6ec3e582599eb50fcd879f0f0105952f3d8b1f8
Thijs Boerefijn <tboefijnb2@gmail.com>
2024-06-05 15:13:38 +0200

    fix: merge artifacts

--- aplib.net-demo/Assets/Scripts/Entities/Enemy/AbstractEnemy.cs
+++ aplib.net-demo/Assets/Scripts/Entities/Enemy/AbstractEnemy.cs
@@ -38,7 +38,6 @@ public abstract class AbstractEnemy : MonoBehaviour
 
         Rigidbody rb = GetComponent<Rigidbody>();
         rb.drag = 2;
-        rb.freezeRotation = true;
         rb.constraints = RigidbodyConstraints.FreezeRotationY;
     }
 
