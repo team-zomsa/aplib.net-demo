0e0645dd60e1332333f3ff76ff2f746354e63542
Thijs Boerefijn <tboefijnb2@gmail.com>
2024-06-10 14:58:37 +0200

    feat: add GridConfig

---
+++ aplib.net-demo/Assets/Scripts/WFC/GridConfig.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Wfc
+{
+    /// <summary>
+    /// Represents the grid config
+    /// </summary>
+    [CreateAssetMenu(fileName = "GridConfig", menuName = "ScriptableObjects/GridConfig", order = 1)]
+    public class GridConfig : ScriptableObject
+    {
+        /// <summary>
+        /// Represents the use of a seed.
+        /// </summary>
+        public bool UseSeed;
+
+        /// <summary>
+        /// Represents the seed.
+        /// </summary>
+        public int Seed;
+
+        /// <summary>
+        /// Represents the width of the grid in the x-direction.
+        /// </summary>
+        public int GridWidthX = 10;
+
+        /// <summary>
+        /// Represents the height of the grid in the z-direction.
+        /// </summary>
+        public int GridWidthZ = 10;
+
+        /// <summary>
+        /// Represents the amount of rooms.
+        /// </summary>
+        public int AmountOfRooms = 5;
+    }
+}
