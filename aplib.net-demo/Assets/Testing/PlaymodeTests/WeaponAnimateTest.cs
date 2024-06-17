71da9e2b976c8f18b91343f86b380271d6902b9d
Quake <QuakeEye@users.noreply.GitHub.com>
2024-06-17 14:57:50 +0200

    test: add test for weapon can animate

---
+++ aplib.net-demo/Assets/Testing/PlaymodeTests/WeaponAnimateTest.cs
@@ -0,0 +1,25 @@
+using Entities.Weapons;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class WeaponAnimateTest
+{
+    [UnityTest]
+    public IEnumerator WeaponAnimateTestWithEnumeratorPasses()
+    {
+        SceneManager.LoadScene("WeaponAnimateTestScene");
+        yield return null;
+
+        // Get the players crossbow
+        var player = GameObject.Find("PlayerAnimated");
+        RangedWeapon crossbow = player.GetComponentInChildren<RangedWeapon>();
+
+        bool canBeAnimated = crossbow.CanAnimate();
+
+        // Check if the crossbow can be animated
+        Assert.IsTrue(canBeAnimated);
+    }
+}
