796cfb521f2047cc6b0301865fc3b5abe942bd98
Joen <90771356+JoenvandeVorle@users.noreply.github.com>
2024-06-19 16:22:03 +0200

    feat: add ranged enemy animations (#100)
    * feat: add animations
    
    * feat: animations added to enemy
    
    * feat: fix walking animations
    
    * fix: ranged weapon positions
    
    * fix: change scene name in canvasmanager
    
    * chore: define variables
    
    * fix: respawning and melee weapon attack
    
    * fix: ranged enemy animations work again after merge
    
    * fix: no more double damage bug
    
    by separating the wall collider on the player, and putting it in the "Ignore Raycast" layer
    
    * chore: punctuation in comments

--- aplib.net-demo/Assets/Scripts/Entities/Enemy/RangedEnemy.cs
+++ aplib.net-demo/Assets/Scripts/Entities/Enemy/RangedEnemy.cs
@@ -1,4 +1,5 @@
 using Entities.Weapons;
+using System.Collections;
 using UnityEngine;
 
 /// <summary>
@@ -6,8 +7,12 @@ using UnityEngine;
 /// Has a separate vision range within which it will move closer to the player.
 /// </summary>
 [RequireComponent(typeof(Timer))]
+[RequireComponent(typeof(Animator))]
 public class RangedEnemy : RespawningEnemy
 {
+    [SerializeField]
+    private float _attackChargeUp = 1f;
+
     [SerializeField]
     private float _attackCooldown = 2f;
 
@@ -18,6 +23,10 @@ public class RangedEnemy : RespawningEnemy
 
     private RangedWeapon _rangedWeapon;
 
+    private Animator _animator;
+
+    private const float _arbitrarySmallStoppingDistance = 1f;
+
     /// <summary>
     /// Initialize the ranged weapon and pathfinding.
     /// Do in start to ensure the weapon/pathfinding is initialized beforehand.
@@ -27,8 +36,9 @@ public class RangedEnemy : RespawningEnemy
         _rangedWeapon = GetComponentInChildren<RangedWeapon>();
         _rangedWeapon.Initialize(_damagePoints, _targetTag, transform, _attackRange);
         _attackTimer = gameObject.AddComponent<Timer>();
-        _attackTimer.SetExactTime(_attackCooldown);
-        _pathFind.SetStoppingDistance(_attackRange - 1f);
+        _attackTimer.SetExactTime(_attackCooldown + _attackChargeUp);
+        _pathFind.SetStoppingDistance(_attackRange - _arbitrarySmallStoppingDistance);
+        _animator = GetComponent<Animator>();
 
         base.Start();
     }
@@ -42,15 +52,30 @@ public class RangedEnemy : RespawningEnemy
         Debug.DrawRay(transform.position, Vector3.up * 20, Color.green);
 
         // If the target is not directly visible but within vision range, move closer to the target.
-        if (!_rangedWeapon.EnemiesInLineOfSight() && _pathFind.GoalWithinRange(_visionRange)) _pathFind.SetStoppingDistance(1f);
+        if (_canMove && !_rangedWeapon.EnemiesInLineOfSight() && _pathFind.GoalWithinRange(_visionRange))
+        {
+            _pathFind.SetStoppingDistance(_arbitrarySmallStoppingDistance);
+            _animator.SetBool("Walking", true);
+        }
 
         if (_attackTimer.IsFinished() && _rangedWeapon.EnemiesInLineOfSight())
         {
-            _pathFind.SetStoppingDistance(_attackRange - 1f);
-            _rangedWeapon.UseWeapon();
-            _attackTimer.Reset();
+            _animator.SetBool("Walking", false);
+            _pathFind.SetStoppingDistance(_attackRange - _arbitrarySmallStoppingDistance);
+            StartCoroutine(Attack());
         }
 
         base.Update();
+    }
+
+    private IEnumerator Attack()
+    {
+        _attackTimer.Reset();
+        _animator.SetBool("Loading", true);
+
+        yield return new WaitForSeconds(_attackChargeUp);
+
+        _animator.SetBool("Loading", false);
+        _rangedWeapon.UseWeapon();
     }
 }
